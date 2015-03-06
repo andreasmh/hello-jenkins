f8bea5a5846ad8f49f81c55c1e28bf07b9ce10d6
andreasmh <andreas.moregard@gmail.com>
2015-03-06 12:46:47 +0100

    Changed test to be correct.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello everyone', function(done) {
-    request(app).get('/').expect('hello everyone', done);
+  it('respond with hello everybody!', function(done) {
+    request(app).get('/').expect('hello everybody!', done);
   });
 });
